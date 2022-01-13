522aa0dfe74eed064d5dbd7244f7c1a1caa44ace
Arseniy <arsen305098@gmail.com>
2022-01-14 00:06:36 +0300

    feat: add task solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+    let positiveNumber = Math.abs(n);
+    let array = String(positiveNumber).split('');
+    let arrayReverse = array.reverse();
+    return arrayReverse.join('');
+};
